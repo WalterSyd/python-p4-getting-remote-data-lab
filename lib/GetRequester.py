2e893acbb3b0b49d9c0c076b286efd72f47968c1
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-10-03 16:06:00 +0300

    implemented get_response_body and load_json methods under GetRequester class

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,17 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
 
+    def get_response_body(self):
+        #send a GET request to the url
+        response = requests.get(self.url)
+        #return the content of the response
+        return response.content
+
+
+#pass the response body to the load_json function to convert it to a JSON object
     def load_json(self):
-        pass
+        #send a GET request to the url
+        response = requests.get(self.url)
+        #return the content of the response in an object form
+        return json.loads(response.content)
